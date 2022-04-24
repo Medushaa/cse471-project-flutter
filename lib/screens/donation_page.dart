8a0335aadb07b6fd57b922b46e8a2974cec342a7
Masuda <Masudarahman27@gmail.com>
2022-04-24 12:19:59 +0600

    updated donation_page.dart

--- lib/screens/donation_page.dart
+++ lib/screens/donation_page.dart
@@ -7,7 +7,7 @@ class DonationPage extends StatelessWidget {
           toolbarHeight: 70,
           backgroundColor: Color.fromARGB(255, 238, 127, 0),
           leading: Icon(Icons.favorite),
-          title: Text('Donation  Bhallage Page',
+          title: Text('Donation Page',
             style: TextStyle(
               fontSize: 25,
             ),
